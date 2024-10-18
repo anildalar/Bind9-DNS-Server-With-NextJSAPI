c396a0c4e584a9cfdaae7bab3c3e04316bc15af5
root <root@vmi2076094.contaboserver.net>
2024-10-18 15:43:08 +0200

    new

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Start the Next.js application with PM2 in watch mode
-pm2 start npm --name "nextjs-app" -- start --watch
+#pm2 start npm --name "nextjs-app" -- start
+#pm2 start npm --name "nextjs-app" -- start --watch
+pm2 start npm --name "nextjs-app" -- run dev --watch
 
 # Wait for PM2 to initialize the app (optional, depending on app startup time)
 # sleep 5
